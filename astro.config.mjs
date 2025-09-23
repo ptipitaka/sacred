263291a80f095f90be58e9c23a10e18b9b69edc3
ptipitaka <contact@ptipitaka.org>
2025-09-23 22:29:34 +0700

    fix components import error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,56 +5,66 @@ import { sidebarConfig } from './python/md/navigator.js'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'SACRED',
-			description: 'Scripture Archive of Canonical References, Editions, Digital corpus',
-			social: [{ 
-				icon: 'github', 
-				label: 'GitHub', 
-				href: 'https://github.com/withastro/starlight' 
-			}],
+    vite: {
+        resolve: {
+            alias: {
+                '@components': new URL('./src/components', import.meta.url).pathname,
+            }
+        }, 
+        optimizeDeps: {
+            include: ['@astrojs/starlight']
+        },               
+    },
+    integrations: [
+        starlight({
+            title: 'SACRED',
+            description: 'Scripture Archive of Canonical References, Editions, Digital corpus',
+            social: [{ 
+                icon: 'github', 
+                label: 'GitHub', 
+                href: 'https://github.com/withastro/starlight' 
+            }],
             plugins: [starlightThemeRapide()],
-			customCss: [
-				'./src/assets/css/global.css',
-				'./src/assets/css/fonts.css',
-			],
-			defaultLocale: 'romn',
-			locales: {
-				mymr: {
-					label: 'မြန်မာ',
-					lang: 'my',
-				},
-				thai: {
-					label: 'ไทย',
-					lang: 'th',
-				},
-				sinh: {
-					label: 'සිංහල',
-					lang: 'si',
-				},
-				romn: {
-					label: 'Roman',
-					lang: 'en',
-				},
-				deva: {
-					label: 'देवनागरी',
-					lang: 'hi',
-				},
-				khmr: {
-					label: 'ខ្មែរ',
-					lang: 'kh',
-				},
-				laoo: {
-					label: 'ລາວ',
-					lang: 'lo',
-				},
-				lana: {
-					label: 'ᩃᨶ᩠ᨶ',
-					lang: 'ln',
-				},
-			},
+            customCss: [
+                './src/assets/css/global.css',
+                './src/assets/css/fonts.css',
+            ],
+            defaultLocale: 'romn',
+            locales: {
+                mymr: {
+                    label: 'မြန်မာ',
+                    lang: 'my',
+                },
+                thai: {
+                    label: 'ไทย',
+                    lang: 'th',
+                },
+                sinh: {
+                    label: 'සිංහල',
+                    lang: 'si',
+                },
+                romn: {
+                    label: 'Roman',
+                    lang: 'en',
+                },
+                deva: {
+                    label: 'देवनागरी',
+                    lang: 'hi',
+                },
+                khmr: {
+                    label: 'ខ្មែរ',
+                    lang: 'kh',
+                },
+                laoo: {
+                    label: 'ລາວ',
+                    lang: 'lo',
+                },
+                lana: {
+                    label: 'ᩃᨶ᩠ᨶ',
+                    lang: 'ln',
+                },
+            },
             sidebar: sidebarConfig
-		}),
-	],
+        }),
+    ],
 });
