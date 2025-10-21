6a596484017eb5ab0045dfe11145229f4e52f376
ptipitaka <contact@ptipitaka.org>
2025-10-21 19:15:22 +0700

    Migrate 18Kh mymr

--- python/md/migrate_batch.ps1
+++ python/md/migrate_batch.ps1
@@ -86,7 +86,7 @@ $tasks = @(
     # @{ Book='17A8'; Review='a8' },
     # @{ Book='17A9'; Review='a9' },
     # @{ Book='17A10'; Review='a10' },
-    @{ Book='17A11'; Review='a11' },
+    # @{ Book='17A11'; Review='a11' },
     @{ Book='18Kh'; Review='kh' },
     @{ Book='18Dh'; Review='dh' },
     @{ Book='18Ud'; Review='ud' },
