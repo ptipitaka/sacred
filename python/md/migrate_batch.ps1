a9437cdc7114d3d74bcbf529940d187fc56b5ca3
ptipitaka <contact@ptipitaka.org>
2025-10-22 00:30:51 +0700

    Migrate 30Vbh mymr

--- python/md/migrate_batch.ps1
+++ python/md/migrate_batch.ps1
@@ -108,7 +108,7 @@ $tasks = @(
     # @{ Book='27Ne'; Review='ne' },
     # @{ Book='27Pe'; Review='pe' },
     # @{ Book='28Mi'; Review='mi' },
-    @{ Book='29Dhs'; Review='dhs' },
+    # @{ Book='29Dhs'; Review='dhs' },
     @{ Book='30Vbh'; Review='vbh' },
     @{ Book='31Dht'; Review='dht' },
     @{ Book='31Pu'; Review='pu' },
