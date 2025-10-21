7734464b11cff42fe7f760aba00814b007af07a9
ptipitaka <contact@ptipitaka.org>
2025-10-21 15:50:51 +0700

    Migrate 4V mymr

--- python/md/migrate_batch.ps1
+++ python/md/migrate_batch.ps1
@@ -33,7 +33,7 @@ $locale = 'mymr'
 $tasks = @(
     # @{ Book='1V'; Review='para' },
     # @{ Book='2V'; Review='paci' },
-    @{ Book='3V'; Review='vi-maha' },
+    # @{ Book='3V'; Review='vi-maha' },
     @{ Book='4V'; Review='cula' },
     @{ Book='5V'; Review='pari' },
     @{ Book='6D'; Review='sila' },
