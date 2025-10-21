a0e72b279e3c8ad97363a913475dc1ce3880f081
ptipitaka <contact@ptipitaka.org>
2025-10-22 03:54:01 +0700

    Migrate 34Y7 mymr

--- python/md/migrate_batch.ps1
+++ python/md/migrate_batch.ps1
@@ -118,7 +118,7 @@ $tasks = @(
     # @{ Book='33Y3'; Review='y3' },
     # @{ Book='33Y4'; Review='y4' },
     # @{ Book='33Y5'; Review='y5' },
-    @{ Book='34Y6'; Review='y6' },
+    # @{ Book='34Y6'; Review='y6' },
     @{ Book='34Y7'; Review='y7' },
     @{ Book='34Y8'; Review='y8' },
     @{ Book='35Y9'; Review='y9' },
