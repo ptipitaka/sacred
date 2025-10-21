4bb4dddd2cc5d9dc08bbfbd14826f487d7f52adb
ptipitaka <contact@ptipitaka.org>
2025-10-21 18:42:34 +0700

    Migrate 17A9 mymr

--- python/md/migrate_batch.ps1
+++ python/md/migrate_batch.ps1
@@ -83,7 +83,7 @@ $tasks = @(
     # @{ Book='16A5'; Review='a5' },
     # @{ Book='16A6'; Review='a6' },
     # @{ Book='16A7'; Review='a7' },
-    @{ Book='17A8'; Review='a8' },
+    # @{ Book='17A8'; Review='a8' },
     @{ Book='17A9'; Review='a9' },
     @{ Book='17A10'; Review='a10' },
     @{ Book='17A11'; Review='a11' },
